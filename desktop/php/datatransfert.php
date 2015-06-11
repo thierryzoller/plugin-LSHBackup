515aa501eb63e4d1e0fcc4d22eac3bac9e0d86d6
loic <lgevrey@oxya.com>
2015-06-11 10:41:31 +0200

    UI improvement

--- desktop/php/datatransfert.php
+++ desktop/php/datatransfert.php
@@ -17,21 +17,21 @@ foreach ($eqLogics as $eqLogic) {
 	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
 }
 ?>
-            </ul>
-        </div>
-    </div>
+           </ul>
+       </div>
+   </div>
 
-    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-        <legend>{{Mes équipements datatransfert}}
-        </legend>
-            <div class="eqLogicThumbnailContainer">
-                  <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-       <center>
-        <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-    </center>
-    <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
-</div>
-                <?php
+   <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+    <legend>{{Mes équipements datatransfert}}
+    </legend>
+    <div class="eqLogicThumbnailContainer">
+      <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+         <center>
+            <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+        </center>
+        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+    </div>
+    <?php
 foreach ($eqLogics as $eqLogic) {
 	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
 	echo "<center>";
@@ -41,94 +41,91 @@ foreach ($eqLogics as $eqLogic) {
 	echo '</div>';
 }
 ?>
-            </div>
-    </div>
+</div>
+</div>
 
-    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-        <div class="row">
-            <div class="col-sm-6">
-                <form class="form-horizontal">
-                    <fieldset>
-                        <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label">{{Nom de l'équipement Data transfert}}</label>
-                            <div class="col-sm-4">
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement Data transfert}}"/>
-                            </div>
+<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+    <div class="row">
+        <div class="col-sm-6">
+            <form class="form-horizontal">
+                <fieldset>
+                    <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">{{Nom de l'équipement Data transfert}}</label>
+                        <div class="col-sm-4">
+                            <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                            <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement Data transfert}}"/>
                         </div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label" >{{Objet parent}}</label>
-                            <div class="col-sm-6">
-                                <select class="eqLogicAttr form-control" data-l1key="object_id">
-                                    <option value="">{{Aucun}}</option>
-                                    <?php
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                        <div class="col-sm-6">
+                            <select class="eqLogicAttr form-control" data-l1key="object_id">
+                                <option value="">{{Aucun}}</option>
+                                <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label">{{Activer}}</label>
-                            <div class="col-sm-1">
-                                <input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>
-                            </div>
-                            <label class="col-sm-4 control-label">{{Visible}}</label>
-                            <div class="col-sm-1">
-                                <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label">{{Type}}</label>
-                            <div class="col-sm-4">
-                                <select class="form-control eqLogicAttr" data-l1key="configuration" data-l2key="protocol">
-                                    <?php
+                           </select>
+                       </div>
+                   </div>
+                   <div class="form-group">
+                    <label class="col-sm-3 control-label"></label>
+                    <div class="col-sm-9">
+                       <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
+                       <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+                   </div>
+               </div>
+               <div class="form-group">
+                <label class="col-sm-3 control-label">{{Type}}</label>
+                <div class="col-sm-4">
+                    <select class="form-control eqLogicAttr" data-l1key="configuration" data-l2key="protocol">
+                        <?php
 foreach (datatransfert::supportedProtocol() as $protocol) {
 	echo '<option value="' . $protocol . '">' . $protocol . '</option>';
 }
 ?>
-                                </select>
-                            </div>
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-            <div class="col-sm-6">
-                <form class="form-horizontal">
-                    <fieldset>
-                        <legend>{{Paramètres}}</legend>
-                        <div id="div_protocolParameters"></div>
-                    </fieldset>
-                </form>
-            </div>
+                   </select>
+               </div>
+           </div>
+       </fieldset>
+   </form>
+</div>
+<div class="col-sm-6">
+    <form class="form-horizontal">
+        <fieldset>
+            <legend>{{Paramètres}}</legend>
+            <div id="div_protocolParameters"></div>
+        </fieldset>
+    </form>
+</div>
+</div>
+<legend>{{Data transfert}}</legend>
+
+
+<a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une commande Data transfert}}</a><br/><br/>
+<table id="table_cmd" class="table table-bordered table-condensed">
+    <thead>
+        <tr>
+            <th>{{Nom}}</th><th>{{Source}}</th><th>{{Cible}}</th><th>{{Filtre}}</th><th></th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+</table>
+
+<form class="form-horizontal">
+    <fieldset>
+        <div class="form-actions">
+            <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+            <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
         </div>
-        <legend>{{Data transfert}}</legend>
+    </fieldset>
+</form>
 
-
-        <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une commande Data transfert}}</a><br/><br/>
-        <table id="table_cmd" class="table table-bordered table-condensed">
-            <thead>
-                <tr>
-                    <th>{{Nom}}</th><th>{{Source}}</th><th>{{Cible}}</th><th>{{Filtre}}</th><th></th>
-                </tr>
-            </thead>
-            <tbody>
-
-            </tbody>
-        </table>
-
-        <form class="form-horizontal">
-            <fieldset>
-                <div class="form-actions">
-                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-                </div>
-            </fieldset>
-        </form>
-
-    </div>
+</div>
 </div>
 
 <?php include_file('desktop', 'datatransfert', 'js', 'datatransfert');?>
