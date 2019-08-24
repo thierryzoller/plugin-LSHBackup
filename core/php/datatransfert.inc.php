0397ecaf65e964423e2280eecce8e62e39087ec6
Zoller Thierry <thierry@zoller.lu>
2019-08-24 11:28:28 +0200

    Update datatransfert.inc.php

--- core/php/datatransfert.inc.php
+++ core/php/datatransfert.inc.php
@@ -44,12 +44,12 @@ class DataTransfert {
     
 	
 	$formats = array(
-	"*-*-*.*.*-Y-m-d-H?i.*.*" => "backup-fensoft-3.1.5-2017-10-04-11h52.tar.gz",
+	//"*-*-*.*.*-Y-m-d-H?i.*.*" => "backup-fensoft-3.1.5-2017-10-04-11h52.tar.gz",
 	"*-*-*.*.*-Y-m-d-H?i.??" => "backup-Moutfort-3.3.33-2019-08-23-21h16.7z",
 	);
 
 	  
-    foreach ($formats as $format => $example) {
+    foreach ($formats as $format) {
 
       $date = \DateTime::createFromFormat($format, $_name);
 
